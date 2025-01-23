d1ab1160f9e78a1a14e527e78b26474e9116dd87
ruddock321 <kienanruddock@gmail.com>
2025-01-23 17:04:05 +0000

    Small updates

--- dev/GarstecModel_V7/GarstecNN_V7_Script_1GPU.py
+++ dev/GarstecModel_V7/GarstecNN_V7_Script_1GPU.py
@@ -209,14 +209,14 @@ class GarstecNet(LightningModule):
         x, y = batch
         y_pred = self(x)
         loss = self.criterion(y_pred, y)
-        self.log('train_loss', loss, on_step=True, on_epoch=True, prog_bar=True)
+        self.log('train_loss', loss, on_step=True, on_epoch=True, prog_bar=False)
         return loss
 
     def validation_step(self, batch, batch_idx):
         x, y = batch
         y_pred = self(x)
         loss = self.criterion(y_pred, y)
-        self.log('val_loss', loss, on_epoch=True, prog_bar=True)
+        self.log('val_loss', loss, on_epoch=True, prog_bar=False)
         return loss
 
     def configure_optimizers(self):
