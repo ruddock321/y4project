d33e241bbf49ad732cde7a7cd5c6f565e2630245
Tom-green1 <tomhgreen@hotmail.com>
2025-03-08 14:42:18 +0000

    new folders plus ran some emus and hares

---
+++ HBM stuff/EMU and HARE plots/Emu and Hare data.py
@@ -0,0 +1,27 @@
+"""
+predicted values for tracks from Bayesian model V9 (mean):
+
+Model, real values
+
+track: 01975: 4 chains, 16000 samples per chain ** HARE not EMU
+
+massini: 0.8779942, 0.869
+mlt: 1.3136264, 1.61523438
+eta: 0.14696938, 0.15864258
+yini: 0.28090766, 0.25497559
+fehini: -1.0163645, -0.70556641
+Fe: 0.20798922, 0.2
+
+"""
+
+"""
+track: 08968 ** EMU not HARE
+
+massini: 0.831578, 0.854
+mlt: 1.9226811, 1.53208008
+eta: 0.14636502, 0.15650024
+yini: 0.29126415, 0.27439148
+fehini: -1.5670056, -1.6260376
+Fe: 0.13738523, 0.2
+
+"""
